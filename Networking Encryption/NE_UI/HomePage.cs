f6ee9aa8279429fc69dff44887d89bb000937878
ffrank0612 <ffrancisc92@gmail.com>
2018-05-30 23:03:08 -0700

    UI Class Diagram

---
+++ Networking Encryption/NE_UI/HomePage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NE_UI
+{
+    public class HomePage : UI_Interface
+    {
+    }
+}
---
+++ Networking Encryption/NE_UI/UI_Interface.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NE_UI
+{
+    public interface UI_Interface
+    {
+    }
+}
